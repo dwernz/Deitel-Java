e4133a6cf1dbd4eae0264c27af849c47352e32b3
Daniel <62781672+dwernz@users.noreply.github.com>
2024-01-02 15:55:20 -0500

    update: Account and AccountTest classes

--- Chapter_7/Account2.java
+++ Chapter_7/Account2.java
@@ -4,5 +4,20 @@ package Chapter_7;
 // Account class with a constructor that initializes the name.
 
 public class Account2 {
+    private String name; // instance variable
 
-}
+    // constructor initializes name with parameter name
+    public Account2(String name) { // constructor name is class name
+        this.name = name;
+    } // end constructor Account2
+
+    // method to set the name
+    public void setName(String name) {
+        this.name = name;
+    } // end method setName
+
+    // method to retrieve the name
+    public String getName() {
+        return name;
+    } // end method getName
+} // end class Account2
